a5f1d65db6492f628de3ddc69cac1ce700a279ad
suchafool <beyondsht@126.com>
2015-04-28 20:42:16 +0800

    added customized copy of controllers, instead of file links.

---
+++ TestApplicatoinV2_0_RAZOR/Controllers/DefaultController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestApplicationv2_0.Models;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson;
+
+namespace TestApplicationv2_0.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //
+        // GET: /Default/
+
+        public ActionResult Index()
+        {
+            Session["value"] = "Hi";
+            return View();
+        }
+
+        public ActionResult PrintSessionVal()
+        {
+            string val = (Session["value"] == null) ? "" : Session["value"].ToString();
+            ViewBag.sessionVal = val;
+            return View();
+        }
+
+        public ActionResult PrintSessionValDouble()
+        {
+            var val = Session.Mongo<double>("value");
+            double dobVal = (double)BsonTypeMapper.MapToDotNetValue(val);
+            ViewBag.sessionVal = dobVal.ToString("G");
+            return View("~/Views/Default/PrintSessionVal.cshtml");
+        }
+
+        public ActionResult SetSessionValInt(int newSesVal = 0)
+        {
+            Session["value"] = newSesVal;
+            return View("~/Views/Default/SetSessionVal.cshtml");
+        }
+
+        public ActionResult SetSessionValBool(bool newSesVal = false)
+        {
+            Session["value"] = newSesVal;
+            return View("~/Views/Default/SetSessionVal.cshtml");
+        }
+
+        public ActionResult SetSessionValDouble()
+        {
+            double newSesVal = 3.1416F;
+            Session["value"] = newSesVal;
+            return View("~/Views/Default/SetSessionVal.cshtml");
+        }
+
+        public ActionResult SetSessionVal(string newSesVal = "")
+        {
+            Session["value"] = newSesVal;
+            return View();
+        }
+
+        public ActionResult SessionAbandon()
+        {
+            Session.Abandon();
+            return View();
+        }
+
+        public ActionResult SerializePerson(
+            string name = "Marc",
+            string surname = "Cortada",
+            string city = "Barcelona")
+        {
+            Person p = new Person()
+            {
+                Name = name,
+                Surname = surname,
+                City = city
+            };
+
+            Session["person"] = p;
+
+            return View();
+        }
+
+        public ActionResult GetSerializedPerson()
+        {
+            Person p = new Person();
+            if (Session["person"] != null)
+            {
+                var obj = Session["person"] as BsonDocument;
+                if (obj != null)
+                    p = BsonSerializer.Deserialize<Person>(obj);
+            }
+            return View(p);
+        }
+
+        public ActionResult SerializePersonWithLists(
+            string name = "Marc",
+            string surname = "Cortada",
+            string city = "Barcelona")
+        {
+            PersonPetsList p = new PersonPetsList()
+            {
+                Name = name,
+                Surname = surname,
+                City = city,
+                PetsList = new List<string>() { "Dog", "Cat", "Shark" }
+            };
+
+            Session["personWithPetsList"] = p;
+
+            return View();
+        }
+
+        public ActionResult GetSerializedPersonWithPets()
+        {
+            PersonPetsList p = new PersonPetsList();
+            if (Session["personWithPetsList"] != null)
+            {
+                var obj = Session["personWithPetsList"] as BsonDocument;
+                if (obj != null)
+                    p = BsonSerializer.Deserialize<PersonPetsList>(obj);
+            }
+            return View(p);
+        }
+
+    }
+}
---
+++ TestApplicatoinV2_0_RAZOR/Controllers/DefaultWithHelpersController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestApplicationv2_0.Models;
+
+namespace TestApplicationv2_0.Controllers
+{
+    public class DefaultWithHelpersController : Controller
+    {
+        //
+        // GET: /DefaultWithHelpers/
+
+        private const string KEY_NAME = "value";
+        private const string KEY_NAME2 = "value2";
+        private const string KEY_NAME3 = "value3";
+        public const string VIEW_DATA_VAL = "sessionVal";
+
+        public ActionResult Index()
+        {
+            Session.Mongo<string>(KEY_NAME, "Hi");
+            return View();
+        }
+
+        public ActionResult GetAndSetSameRequest()
+        {
+            Person personSetted = new Person() { Name = "Marc", Surname = "Cortada", City = "Barcelona" };
+            Session.Mongo<Person>(KEY_NAME, personSetted);
+
+            PersonPetsList personPetsSetted = new PersonPetsList()
+            {
+                Name = "Marc2",
+                Surname = "Cortada2",
+                PetsList = new List<string>() { "cat", "dog" }
+            };
+            Session.Mongo<PersonPetsList>(KEY_NAME3, personPetsSetted);
+
+            int settedValInt = 3;
+            Session.Mongo<int>(KEY_NAME2, settedValInt);
+
+            Person personGetted = Session.Mongo<Person>(KEY_NAME);
+            PersonPetsList personPetsListGetted = Session.Mongo<PersonPetsList>(KEY_NAME3);
+            int gettedValInt = Session.Mongo<int>(KEY_NAME2);
+
+            if ((settedValInt == gettedValInt) &&
+                (personSetted.Name == personGetted.Name) &&
+                (personSetted.Surname == personGetted.Surname) &&
+                (personSetted.City == personGetted.City) &&
+                (personPetsListGetted.Name == personPetsSetted.Name) &&
+                (personPetsListGetted.Surname == personPetsSetted.Surname) &&
+                (personPetsListGetted.PetsList[0] == personPetsSetted.PetsList[0]) &&
+                (personPetsListGetted.PetsList[1] == personPetsSetted.PetsList[1]))
+                ViewBag.allOk = "True";
+            else
+                ViewBag.allOk = "False";
+
+            return View();
+        }
+
+        public ActionResult PrintSessionSerializedPerson()
+        {
+            Person p = Session.Mongo<Person>(KEY_NAME);
+            return View("~/Views/Default/GetSerializedPerson.cshmtl", p);
+        }
+
+        public ActionResult PrintSessionSerializedPersonWithlist()
+        {
+            PersonPetsList p = Session.Mongo<PersonPetsList>(KEY_NAME3);
+            return View("~/Views/Default/GetSerializedPersonWithPets.cshtml", p);
+        }
+
+        public ActionResult SetSessionValString(string newSesVal = "")
+        {
+            Session.Mongo<string>(KEY_NAME, newSesVal);
+
+            return View("~/Views/Default/SetSessionVal.cshtml");
+        }
+
+        public ActionResult PrintSessionValString()
+        {
+            string val = Session.Mongo<string>(KEY_NAME);
+
+            ViewBag.sessionVal = val;
+            return View("~/Views/Default/PrintSessionVal.cshtml");
+        }
+
+        public ActionResult SetSessionValDouble()
+        {
+            double newSesVal = 3.1416F;
+            Session.Mongo<double>(KEY_NAME, newSesVal);
+
+            return View("~/Views/Default/SetSessionVal.cshtml");
+        }
+
+        public ActionResult PrintSessionValDouble()
+        {
+            double dobVal = Session.Mongo<double>(KEY_NAME);
+
+            ViewBag.sessionVal = string.Format("{0:0.0000}", dobVal);
+            return View("~/Views/Default/PrintSessionVal.cshtml");
+        }
+
+        public ActionResult SetSessionValInt(int newSesVal = 0)
+        {
+            Session.Mongo<int>(KEY_NAME, newSesVal);
+
+            return View("~/Views/Default/SetSessionVal.cshtml");
+        }
+
+        public ActionResult PrintSessionValInt()
+        {
+            //to me, when i save an int value into Mongo, I retrive back a long value.
+            long intVal = Session.Mongo<long>(KEY_NAME);
+
+            ViewBag.sessionVal = intVal;
+            return View("~/Views/Default/PrintSessionVal.cshtml");
+        }
+
+        public ActionResult SetSessionValBool(bool newSesVal = false)
+        {
+            Session.Mongo<bool>(KEY_NAME, newSesVal);
+
+            return View("~/Views/Default/SetSessionVal.cshtml");
+        }
+
+        public ActionResult PrintSessionValBool()
+        {
+            bool boolVal = Session.Mongo<bool>(KEY_NAME);
+
+            ViewBag.sessionVal = boolVal;
+            return View("~/Views/Default/PrintSessionVal.cshtml");
+        }
+    }
+}
